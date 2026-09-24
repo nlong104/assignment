819a669db3cfe2ad7c5a706948fff75611c0b52f
Natasha Long <natashal104@icloud.com>
2026-09-24 13:21:01 +0800

    first few lines of app code from lab 6

--- assignment_coding.py
+++ assignment_coding.py
@@ -1,3 +1,3 @@
-print("Hello")
-print("add")
-print("another new line")
+import csv, json, os # modules built into Python (no install needed)
+from flask import Flask, jsonify, request, send_file # tools taken from the Flask library
+app = Flask(__name__) # create the web application
