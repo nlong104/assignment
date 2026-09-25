c84a939aa4288d84c46eaede259e928dc15f0d5b
Hannah Foley <25268268@student.uwa.edu.au>
2026-09-25 16:26:57 +0800

    Changing names

--- assignment_coding.py
+++
@@ -1,3 +0,0 @@
-import csv, json, os # modules built into Python (no install needed)
-from flask import Flask, jsonify, request, send_file # tools taken from the Flask library
-app = Flask(__name__) # create the web application
